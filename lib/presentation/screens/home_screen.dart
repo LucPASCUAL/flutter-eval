3e6f90effefecf33f83989120ac477633baa811f
LucPSC <luc.pascual1997@gmail.com>
2025-04-30 13:18:24 +0200

    fix: default structure has been fixed

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo/util/style_util.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,7 +7,12 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: HomeScreen(),
+      body: Center(
+        child: Text(
+          "Bienvenue sur Flutter !",
+          style: StyleUtil.getMainTextStyle(),
+        ),
+      ),
     );
   }
 }
